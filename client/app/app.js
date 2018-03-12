f21428baea158294672b393c520abae50209ffe2
Michal Janda <michal.janda@volny.cz>
2018-03-12 16:14:31 +0100

    notes in panels

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,9 @@
-angular.module('jsTrain', []);
+let app = angular.module('notes', []);
+
+app.controller('NotesList', ($scope, $http) => {
+    $http.get('/api/notes').then((res) => {
+        $scope.notes = res.data;
+    });
+});
+
+
