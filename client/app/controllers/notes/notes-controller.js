9cf0c1465f77e60965efb0960efa6be0964a0d5a
Michal Janda <michal.janda@volny.cz>
2018-03-12 17:07:46 +0100

    single route

--- client/app/controllers/notes/notes-controller.js
+++ client/app/controllers/notes/notes-controller.js
@@ -1,6 +1,7 @@
 angular.module('notes').controller('NotesCtrl', ($scope, notesService) => {
-  console.log('jj');
+
   notesService.list().then(res => {
     $scope.notes = res.data;
   });
+  
 });
