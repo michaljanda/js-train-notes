ac63147c4bad14ce18b88fb0b369424b8e012a74
Michal Janda <michal.janda@volny.cz>
2018-03-13 21:36:32 +0100

    progress

--- client/app.js
+++ client/app.js
@@ -3,15 +3,20 @@ let app = angular.module('notes', ['ui.router']);
 app.config(($stateProvider, $urlRouterProvider) => {
 
   $stateProvider
-  .state('notes', {
-    url: '/notes',
-    controller: 'NotesCtrl',
-    templateUrl: 'app/controllers/notes/notes.html'
-  })
-  .state('about', {
-    url: '/about',
-    templateUrl: 'app/controllers/about/about.html'
-  });
+    .state('notes', {
+      url: '/notes',
+      controller: 'NotesCtrl',
+      templateUrl: 'app/controllers/notes/notes.html'
+    })
+    .state('notes.detail', {
+      url: '/:id',
+      templateUrl: 'app/controllers/notes/note-detail.html',
+      controller: 'NoteDetailCtrl'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: 'app/controllers/about/about.html'
+    });
 
   $urlRouterProvider.otherwise('/notes');
 });
--- client/app/controllers/notes/notes-controller.js
+++ client/app/controllers/notes/notes-controller.js
@@ -1,7 +1,14 @@
-angular.module('notes').controller('NotesCtrl', ($scope, notesService) => {
+angular.module('notes').controller('NotesCtrl', ($scope, notesService, $state) => {
 
   notesService.list().then(res => {
     $scope.notes = res.data;
+    if ($state.params.id) {
+      selectDetail();
+    }
   });
+
+  function selectDetail() {
+    $scope.selected = _.find($scope.notes, {id: +$state.params.id});
+  }
   
 });
